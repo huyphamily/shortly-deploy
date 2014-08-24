6315084ff8809aa56870dab22463a02b6c3f367f
huyphamily <huyphamily@gmail.com>
2014-08-23 17:20:53 -0700

    refactor database code to handle both enviroments

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/shortly');
+var pathURI = process.env.MONGO_URI || 'mongodb://localhost/shortly';
+mongoose.connect(pathURI);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
   console.log('Connected to MongoDB');
-  
 })
 
 
